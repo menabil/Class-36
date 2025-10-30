a159689e10570617c1fd16ca9691b93c22b42f1f
menabil <nabil99cc@gmail.com>
2025-10-31 00:03:11 +0600

    pray done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
-// const onEE = document.getElementById("onEE");
-// const tnEE = document.getElementById("tnEE");
-// const ewewe = document.getElementById("ewewe");
-// const rea = document.getElementById("rea");
-// const qea = document.getElementById("qea");
-
-// rea.addEventListener("click", () => {
-//   qea.style.background = "green";
-//   onEE.setAttribute("hidden", true);
-//   tnEE.removeAttribute("hidden", true);
-// });
+// =========================================
 
 let boxOne = document.getElementById("boxOne");
 let rea = document.getElementById("rea");
@@ -17,7 +7,7 @@ let rea = document.getElementById("rea");
 
 rea.addEventListener("click", () => {
   boxOne.setAttribute("hidden", true);
-  // boxTne.removeAttribute("hidden");
+  boxTne.removeAttribute("hidden");
 });
 
 // =========================================
@@ -30,8 +20,17 @@ let erow = document.getElementById("erow");
 // 2========================================
 
 tRea.addEventListener("click", () => {
-  boxTne.setAttribute("hidden", true);
-  // boxTrre.removeAttribute("hidden");
+  let inpValue = inPPP.value;
+  if (inpValue == "") {
+    erow.removeAttribute("hidden");
+    inpValue = "";
+  } else {
+    erow.setAttribute("hidden", true);
+    inpValue = "";
+    boxTne.setAttribute("hidden", true);
+    boxTrre.removeAttribute("hidden");
+    tUwewe.innerText = inPPP.value;
+  }
 });
 
 // =========================================
@@ -45,8 +44,16 @@ let erEw = document.getElementById("erEw");
 // 3========================================
 
 tOea.addEventListener("click", () => {
-  boxTrre.setAttribute("hidden", true);
-  // boxFne.removeAttribute("hidden");
+  let inpValue = inOPP.value;
+  if (inpValue == "") {
+    erEw.removeAttribute("hidden");
+    inpValue = "";
+  } else {
+    erEw.setAttribute("hidden", true);
+    inpValue = "";
+    boxTrre.setAttribute("hidden", true);
+    boxFne.removeAttribute("hidden");
+  }
 });
 
 // =========================================
@@ -59,8 +66,17 @@ let eFow = document.getElementById("eFow");
 // 4========================================
 
 tFea.addEventListener("click", () => {
-  boxFne.setAttribute("hidden", true);
-  // boxVrre.removeAttribute("hidden");
+  let inpValue = inFPP.value;
+  if (inpValue == "") {
+    eFow.removeAttribute("hidden");
+    inpValue = "";
+  } else {
+    eFow.setAttribute("hidden", true);
+    inpValue = "";
+    boxFne.setAttribute("hidden", true);
+    boxVrre.removeAttribute("hidden");
+    tVwewe.innerText = inFPP.value;
+  }
 });
 
 // =========================================
@@ -73,9 +89,17 @@ let eVow = document.getElementById("eVow");
 
 // 5========================================
 
-tOea.addEventListener("click", () => {
-  boxVrre.setAttribute("hidden", true);
-  // boxSne.removeAttribute("hidden");
+tVea.addEventListener("click", () => {
+  let inpValue = inVPP.value;
+  if (inpValue == "") {
+    eVow.removeAttribute("hidden");
+    inpValue = "";
+  } else {
+    eVow.setAttribute("hidden", true);
+    inpValue = "";
+    boxVrre.setAttribute("hidden", true);
+    boxSne.removeAttribute("hidden");
+  }
 });
 
 // =========================================
@@ -86,9 +110,13 @@ let rSa = document.getElementById("rSa");
 
 // 6========================================
 
-rea.addEventListener("click", () => {
-  boxSne.setAttribute("hidden");
-  // boxOne.removeAttribute("hidden");
+rSa.addEventListener("click", () => {
+  boxSne.setAttribute("hidden", true);
+  boxOne.removeAttribute("hidden");
+
+  if (inOPP.value == inVPP.value) {
+    console.log((eSewe.innerText = "Sorry! Player One is Win"));
+  }
 });
 
 // =========================================
