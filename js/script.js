c25bc6ece1e964c79031c0b947edba5720e537e6
menabil <nabil99cc@gmail.com>
2025-10-30 18:25:32 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,94 @@
+// const onEE = document.getElementById("onEE");
+// const tnEE = document.getElementById("tnEE");
+// const ewewe = document.getElementById("ewewe");
+// const rea = document.getElementById("rea");
+// const qea = document.getElementById("qea");
+
+// rea.addEventListener("click", () => {
+//   qea.style.background = "green";
+//   onEE.setAttribute("hidden", true);
+//   tnEE.removeAttribute("hidden", true);
+// });
+
+let boxOne = document.getElementById("boxOne");
+let rea = document.getElementById("rea");
+
+// 1========================================
+
+rea.addEventListener("click", () => {
+  boxOne.setAttribute("hidden", true);
+  // boxTne.removeAttribute("hidden");
+});
+
+// =========================================
+
+let boxTne = document.getElementById("boxTne");
+let inPPP = document.getElementById("inPPP");
+let tRea = document.getElementById("tRea");
+let erow = document.getElementById("erow");
+
+// 2========================================
+
+tRea.addEventListener("click", () => {
+  boxTne.setAttribute("hidden", true);
+  // boxTrre.removeAttribute("hidden");
+});
+
+// =========================================
+
+let boxTrre = document.getElementById("boxTrre");
+let tUwewe = document.getElementById("tUwewe");
+let inOPP = document.getElementById("inOPP");
+let tOea = document.getElementById("tOea");
+let erEw = document.getElementById("erEw");
+
+// 3========================================
+
+tOea.addEventListener("click", () => {
+  boxTrre.setAttribute("hidden", true);
+  // boxFne.removeAttribute("hidden");
+});
+
+// =========================================
+
+let boxFne = document.getElementById("boxFne");
+let inFPP = document.getElementById("inFPP");
+let tFea = document.getElementById("tFea");
+let eFow = document.getElementById("eFow");
+
+// 4========================================
+
+tFea.addEventListener("click", () => {
+  boxFne.setAttribute("hidden", true);
+  // boxVrre.removeAttribute("hidden");
+});
+
+// =========================================
+
+let boxVrre = document.getElementById("boxVrre");
+let tVwewe = document.getElementById("tVwewe");
+let inVPP = document.getElementById("inVPP");
+let tVea = document.getElementById("tVea");
+let eVow = document.getElementById("eVow");
+
+// 5========================================
+
+tOea.addEventListener("click", () => {
+  boxVrre.setAttribute("hidden", true);
+  // boxSne.removeAttribute("hidden");
+});
+
+// =========================================
+
+let boxSne = document.getElementById("boxSne");
+let eSewe = document.getElementById("eSewe");
+let rSa = document.getElementById("rSa");
+
+// 6========================================
+
+rea.addEventListener("click", () => {
+  boxSne.setAttribute("hidden");
+  // boxOne.removeAttribute("hidden");
+});
+
+// =========================================
